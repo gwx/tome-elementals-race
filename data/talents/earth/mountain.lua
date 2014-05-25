2aa0dbc4776efe443a161f9eeea99e933fc101f5
grayswandir <grayswandir76@gmail.com>
2014-05-25 11:28:11 -0400

    Cleanup.

--- data/talents/earth/mountain.lua
+++ data/talents/earth/mountain.lua
@@ -13,6 +13,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 local stats = require 'engine.interface.ActorStats'
 
 newTalentType {
