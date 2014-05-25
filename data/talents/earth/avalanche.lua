dc8380e1f23b2d8416607609e919c2501b1d3024
grayswandir <grayswandir76@gmail.com>
2014-05-25 00:42:28 -0400

    Fixed accuracy value.

--- data/talents/earth/avalanche.lua
+++ data/talents/earth/avalanche.lua
@@ -74,7 +74,7 @@ newTalent {
 	cooldown = 9,
 	tactical = {ATTACK = 3,},
 	accuracy = function(self, t)
-		return self:combatTalentScale(t, 10, 20) * (5 + self:getStr(5, true))
+		return self:combatTalentScale(t, 10, 20) * (0.5 + self:getStr(0.5, true))
 	end,
 	damage = function(self, t)
 		return self:combatTalentPhysicalDamage(t, 50, 300)
