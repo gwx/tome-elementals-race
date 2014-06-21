85020e9a71e03a9a5ebfc38c64e697c69c776d0f
grayswandir <grayswandir76@gmail.com>
2014-06-21 07:30:31 -0400

    Explosion added.

--- data/talents/fire/pyrokinesis.lua
+++ data/talents/fire/pyrokinesis.lua
@@ -65,7 +65,7 @@ newTalent {
 		return true
 	end,
 	info = function(self, t)
-		local damage = self:spellCrit(self:heatScale(util.getval(t.damage, self, t)))
+		local damage = util.getval(t.damage, self, t)
 		return ([[Sets the target on fire, dealing %d <%d> fire damage in radius %d, burning the center target for the same amount of damage each turn for %d turns.
 This recovers %d heat.
 #GREY#Numbers shown are for 100%% heat, numbers in <brackets> are the actual amounts based on your current heat.]])
@@ -126,5 +126,63 @@ newTalent {
 							util.getval(t.radius, self, t),
 							Talents.damDesc(self, 'FIRE', self:heatScale(damage, 100)),
 							Talents.damDesc(self, 'FIRE', self:heatScale(damage)),
+							util.getval(t.heat_gain, self, t))
+	end,}
+
+newTalent {
+	name = 'Explosion',
+	type = {'elemental/pyrokinesis', 3,},
+	require = make_require(3),
+	points = 5,
+	cooldown = 9,
+	heat_gain = 50,
+	damage = function(self, t) return self:combatTalentSpellDamage(t, 30, 160) end,
+	knockback = function(self, t, heat)
+		return 2 + math.floor((heat or self.heat) / 50)
+	end,
+	range = 4,
+	radius = 3,
+	target = function(self, t)
+		return {type = 'ball', talent = t, selffire = false, nowarning = true,
+						range = util.getval(t.range, self, t),
+						radius = util.getval(t.radius, self, t),}
+	end,
+	action = function(self, t)
+		local _
+		local tg = util.getval(t.target, self, t)
+		local x, y, actor = self:getTarget(tg)
+		if not x or not y then return end
+		_, _, _, x, y = self:canProject(tg, x, y)
+
+		local damage = self:spellCrit(self:heatScale(util.getval(t.damage, self, t)))
+		local knockback = util.getval(t.knockback, self, t)
+		local hit = false
+		local projector = function(px, py, tg, self)
+			local actor = game.level.map(px, py, ACTOR)
+			if not actor then return end
+			hit = true
+			damage_type:get('PHYSICAL').projector(self, px, py, 'PHYSICAL', damage)
+			damage_type:get('FIRE').projector(self, px, py, 'FIRE', damage)
+			if px ~= x or py ~= y then actor:knockback(x, y, knockback) end
+		end
+		self:project(tg, x, y, projector)
+
+		if hit then self:incHeat(util.getval(t.heat_gain, self, t)) end
+
+		game.level.map:particleEmitter(x, y, tg.radius, 'fireflash', {radius = tg.radius,})
+		game:playSoundNear(actor, 'talents/fire')
+		return true
+	end,
+	info = function(self, t)
+		local damage = util.getval(t.damage, self, t)
+		return ([[Causes a localized, fiery explosion at target position, dealing %d <%d> physical and %d <%d> fire damage to all targets in radius 3. Those hit are knocked back from the explosion point by %d <%d> tiles.
+This recovers %d heat if it hits anything.
+#GREY#Numbers shown are for 100%% heat, numbers in <brackets> are the actual amounts based on your current heat.]])
+			:format(Talents.damDesc(self, 'PHYSICAL', self:heatScale(damage, 100)),
+							Talents.damDesc(self, 'PHYSICAL', self:heatScale(damage)),
+							Talents.damDesc(self, 'FIRE', self:heatScale(damage, 100)),
+							Talents.damDesc(self, 'FIRE', self:heatScale(damage)),
+							util.getval(t.knockback, self, t, 100),
+							util.getval(t.knockback, self, t),
 							util.getval(t.heat_gain, self, t))
 	end,}
